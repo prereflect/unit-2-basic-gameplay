c2280cd7338eea149d478ed5061d5e9d59eba535
Toby Woller <toby@tobywoller.com>
2022-04-10 11:07:34 -0700

    4. Move the player left-to-right
    > We have to actually use the horizontal input to translate the Player left and right.
    
    1. Declare a new public float speed = 10.0f;
    2. In Update(), Translate the player side-to-side based on horizontalInput and speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal"); 
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
     }
 }
