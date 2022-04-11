8afab33fc3474cf4166153eb98719654015950ff
Toby Woller <toby@tobywoller.com>
2022-04-11 14:04:40 -0700

    5 Keep the player inbounds
    > We have to prevent the player from going off the side of the screen with an if-then statement.
    
        In Update(), write an if-statement checking if the player’s left X position is less than a certain value
        In the if-statement, set the player’s  position to its current position, but with a fixed X location

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
     }
